1bbaf127a4789837e7462a4dacd2cd6204f547a9
BarryGon911 <corragio.sage@gmail.com>
2025-09-15 12:48:29 -0600

    more components created

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Contacto } from './contacto/contacto';
+import { Conocenos } from './conocenos/conocenos';
+import { About } from './about/about';
 
 @NgModule({
-  declarations: [App],
+  declarations: [App, Contacto, Conocenos, About],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners(), provideZonelessChangeDetection()],
   bootstrap: [App]
